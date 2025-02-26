e039422898a604f8619d41a26dc75786af843669
mcfwesh <mcfwesh@gmail.com>
2025-02-25 23:08:21 -0500

    Fix variable interpolation in pushDocker function for consistency

--- script.groovy
+++ script.groovy
@@ -13,7 +13,7 @@ def pushDocker() {
     withCredentials([
         usernamePassword(credentials:'a967aeaf-43d9-49de-a9a1-5725c0918685', usernameVariable: "USER", passwordVariable: "PWD" )
         ]){
-                sh "echo ${PWD} | docker login -u ${USER} --password-stdin"
+                sh "echo $PWD | docker login -u $USER --password-stdin"
                 sh "docker build -t mcfwesh/java-maven-app:1.1 ."
                 sh "docker push mcfwesh/java-maven-app:1.1"
         }
