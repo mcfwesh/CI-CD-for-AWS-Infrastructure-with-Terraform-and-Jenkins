44c975ec01f70756792ac9be647b2cc04f814721
mcfwesh <mcfwesh@gmail.com>
2025-02-26 20:49:14 -0500

    Fix escaping in incrementVersion function for correct version parsing

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def incrementVersion() {
     echo "Incrementing version..."
-    sh 'mvn build-helper:parse-version versions:set
-        -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}
+    sh 'mvn build-helper:parse-version versions:set \
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
         versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
     def version = matcher[0][1]
