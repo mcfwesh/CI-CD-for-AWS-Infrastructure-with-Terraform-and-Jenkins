cfae9fa96c0dd48a4ce2e1642a122c590ea40420
mcfwesh <mcfwesh@gmail.com>
2025-03-10 17:55:09 -0400

    fix: export environment variables for image name and Docker credentials in server-cmds.sh

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IMAGE_NAME=$1
-DOCKER_USER=$2
-DOCKER_PASSWORD=$3
+export IMAGE_NAME=$1
+export DOCKER_USER=$2
+export DOCKER_PASSWORD=$3
 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 docker-compose up -d
