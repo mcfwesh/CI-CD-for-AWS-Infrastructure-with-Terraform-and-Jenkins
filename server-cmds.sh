52690afbffb97d4681318cf5af06d059e20d52bf
mcfwesh <mcfwesh@gmail.com>
2025-03-10 17:37:27 -0400

    fix: add missing space in Docker login command in server-cmds.sh

--- server-cmds.sh
+++ server-cmds.sh
@@ -4,5 +4,5 @@ IMAGE_NAME=$1
 DOCKER_USER=$2
 DOCKER_PASSWORD=$3
 
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER--password-stdin
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 docker-compose up -d
